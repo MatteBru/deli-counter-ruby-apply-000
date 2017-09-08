280c0ff1761e34d2862d6eeb0ca81997db2dd3b7
Matteo Bruni <mbruni616@gmail.com>
2017-09-08 00:08:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 katz_deli = [] # declares line array
 
 
-def line(lin) # accepts line array and puts line status
+def line(lin) # accepts line array as argument and puts line status
   if lin.empty?
     puts "The line is currently empty."
   else
@@ -13,7 +13,7 @@ def line(lin) # accepts line array and puts line status
   end
 end
 
-def take_a_number(lin, name) # puts welcome message, appends name to line array
+def take_a_number(lin, name) # appends name to line array, puts welcome message
   lin << name
   puts "Welcome, #{name}. You are number #{lin.length} in line." # lin.length previously lin.index(name)+1
 end

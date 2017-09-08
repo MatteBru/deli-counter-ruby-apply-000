1a9e1b4e4ef5fb3e447c6fe87c04837f047eaada
Matteo Bruni <mbruni616@gmail.com>
2017-09-08 00:00:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,15 +7,15 @@ def line(lin) # accepts line array and puts line status
   if lin.empty?
     puts "The line is currently empty."
   else
-    s = "The line is currently:" # declares stub string for line status
-    lin.each{|name| s << " #{lin.index(name)+1}. #{name}"} # iterates through line array, appends string w/ number and name to stub
-    puts s
+    stub = "The line is currently:" # declares stub string for line status
+    lin.each{|name| stub << " #{lin.index(name)+1}. #{name}"} # iterates through line array, appends string w/ number and name to stub
+    puts stub
   end
 end
 
 def take_a_number(lin, name) # puts welcome message, appends name to line array
   lin << name
-  puts "Welcome, #{name}. You are number #{lin.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{lin.length} in line."
 end
 
 def now_serving(lin) # checks if line is full, if so puts currently serving message and removes first name in line array
